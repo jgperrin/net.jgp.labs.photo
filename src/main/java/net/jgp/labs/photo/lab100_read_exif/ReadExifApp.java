c7b58d0d3312fe83a1b81db3b94d01ad5317ca80
jgp <jgp@jgp.net>
2020-05-07 19:56:01 -0400

    First lab

--- src/main/java/net/jgp/labs/photo/lab100_read_exif/ReadExifApp.java
+++ src/main/java/net/jgp/labs/photo/lab100_read_exif/ReadExifApp.java
@@ -1,11 +1,16 @@
 package net.jgp.labs.photo.lab100_read_exif;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import java.io.File;
+import java.io.IOException;
+
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 /**
- * Displays the EXIF attributes of a photo or photos.
+ * Displays the EXIF attributes of a specific photo.
  * 
  * @author jgp
  */
@@ -25,19 +30,42 @@ public class ReadExifApp {
    * The processing code.
    */
   private void start() {
-    // Creates a session on a local master
-    SparkSession spark = SparkSession.builder()
-        .appName("CSV to Dataset")
-        .master("local")
-        .getOrCreate();
+    File file = new File("data/obelix-clio-orig.jpeg");
 
-    // Reads a CSV file with header, called books.csv, stores it in a
-    // dataframe
-    Dataset<Row> df = spark.read().format("csv")
-        .option("header", "true")
-        .load("data/books.csv");
+    try {
+      Metadata metadata = JpegMetadataReader.readMetadata(file);
 
-    // Shows at most 5 rows from the dataframe
-    df.show(5);
+      print(metadata, "Using JpegMetadataReader");
+    } catch (JpegProcessingException e) {
+      print(e);
+    } catch (IOException e) {
+      print(e);
+    }
+  }
+
+  private static void print(Metadata metadata, String method) {
+    System.out.println();
+    System.out.println("-------------------------------------------------");
+    System.out.print(' ');
+    System.out.print(method);
+    System.out.println("-------------------------------------------------");
+    System.out.println();
+
+    // A Metadata object contains multiple Directory objects
+    for (Directory directory : metadata.getDirectories()) {
+      // Each Directory stores values in Tag objects
+      for (Tag tag : directory.getTags()) {
+        System.out.println(tag);
+      }
+
+      // Each Directory may also contain error messages
+      for (String error : directory.getErrors()) {
+        System.err.println("ERROR: " + error);
+      }
+    }
+  }
+
+  private static void print(Exception exception) {
+    System.err.println("EXCEPTION: " + exception);
   }
 }
